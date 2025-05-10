1692ea8ccecd59b3b3ccf56cb8ac6c9219f6933f
Utkarsh <itsmeRishab08@gmail.com>
2025-05-10 21:05:16 +0530

    Set up the frontend

---
+++ frontend/src/main.tsx
@@ -0,0 +1,15 @@
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <MantineProvider>
+      <App />
+    </MantineProvider>
+  </StrictMode>,
+)
